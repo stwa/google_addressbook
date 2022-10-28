b7d396df11faea96fdcd72bf54808a11c1e94f83
numericOverflow <numericOverflow@users.noreply.github.com>
2022-10-28 16:13:13 -0500

    Add "IF NOT EXISTS" syntax on create table
    Avoids error if table already exists in DB

--- SQL/sqlite.initial.sql
+++ SQL/sqlite.initial.sql
@@ -2,7 +2,7 @@
 -- Table structure for table contacts_google
 -- 
 
-CREATE TABLE contacts_google (
+CREATE TABLE IF NOT EXISTS contacts_google (
   contact_id integer NOT NULL PRIMARY KEY,
   user_id integer NOT NULL,
   changed datetime NOT NULL default '0000-00-00 00:00:00',
@@ -15,5 +15,5 @@ CREATE TABLE contacts_google (
   words text NOT NULL default ''
 );
 
-CREATE INDEX ix_contacts_google_user_id ON contacts_google(user_id, del);
+CREATE INDEX IF NOT EXISTS ix_contacts_google_user_id ON contacts_google(user_id, del);
 
